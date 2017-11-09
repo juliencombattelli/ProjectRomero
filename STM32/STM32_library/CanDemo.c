afdf133f28f53241d78eb3779ba5aed03d191d86
Cegreteau <c_egreteau@orange.fr>
2017-11-09 17:21:06 +0100

    Add FIFO and mailbox functions

--- STM32/STM32_library/CanDemo.c
+++ STM32/STM32_library/CanDemo.c
@@ -66,11 +66,11 @@ void val_display (void) {
 void can_Init (void) {
 
   CAN_setup ();                                   /* setup CAN Controller     */
-  CAN_wrFilter (0, STANDARD_FORMAT);             /* Enable reception of msgs */
-	CAN_wrFilter (2, STANDARD_FORMAT);             /* Enable reception of msgs */
-	CAN_wrFilter (3, STANDARD_FORMAT);             /* Enable reception of msgs */
-	CAN_wrFilter (4, STANDARD_FORMAT);             /* Enable reception of msgs */
-	CAN_wrFilter (5, STANDARD_FORMAT);             /* Enable reception of msgs */
+  CAN_wrFilter (CAN_ID_RMT_ULTRASOUND, STANDARD_FORMAT);             /* Enable reception of msgs */
+	CAN_wrFilter (CAN_ID_CMD_DIR, STANDARD_FORMAT);             /* Enable reception of msgs */
+	CAN_wrFilter (CAN_ID_CMD_SPEED, STANDARD_FORMAT);             /* Enable reception of msgs */
+	CAN_wrFilter (CAN_ID_RMT_DIR, STANDARD_FORMAT);             /* Enable reception of msgs */
+	CAN_wrFilter (CAN_ID_RMT_SPEED, STANDARD_FORMAT);             /* Enable reception of msgs */
 	
   CAN_start ();                                   /* start CAN Controller   */
 	CAN_TxMsg.id = CAN_ID_ULTRASOUND;
@@ -78,35 +78,73 @@ void can_Init (void) {
 }
 
 void canPeriodic (void) {
-	val_Tx++;
-	CAN_waitReady (); 	
-	switch (CAN_TxMsg.id) {
-		case CAN_ID_ULTRASOUND: 
-			CAN_TxRdy0 = 0;
-			break;
-		case CAN_ID_DIR: 
-			CAN_TxRdy1 = 0;
-			break;
-		case CAN_ID_SPEED: 
-			CAN_TxRdy2 = 0;
-			break;
-		default:
-			break;
-	}
+	int i;
+	
+	/*------------------------------------------
+	 * Send an ultrasound frame every 200ms
+	 *-----------------------------------------*/
+	CAN_TxMsg.id = CAN_ID_ULTRASOUND;                /* initialize msg to send   */  
+  for (i = 0; i < 8; i++) CAN_TxMsg.data[i] = 0;
+  CAN_TxMsg.len = 1;
+  CAN_TxMsg.format = STANDARD_FORMAT;
+  CAN_TxMsg.type = DATA_FRAME;	
+	
+	val_Tx++; //Value of the ultrasound sensors
+	CAN_waitReady ();
+ 	CAN_TxRdy0 = 0;    													/* CAN HW unready to transmit message mailbox 0*/
+	
 	CAN_TxMsg.data[0] = val_Tx;                 /* data[0] = ADC value      */
 	CAN_wrMsg (&CAN_TxMsg);                     /* transmit message         */
 	
-	periodic_modulo++;
-	
-
+	switch (periodic_modulo) {
+		/*------------------------------------------
+	 * Send an direction frame every 600ms
+	 *-----------------------------------------*/
+		case 0:
+			CAN_TxMsg.id = CAN_ID_DIR;                /* initialize msg to send   */  
+			for (i = 0; i < 8; i++) CAN_TxMsg.data[i] = 0;
+			CAN_TxMsg.len = 1;
+			CAN_TxMsg.format = STANDARD_FORMAT;
+			CAN_TxMsg.type = DATA_FRAME;	
+		
+			val_Tx++; //Value of the direction potentiometer
+			CAN_waitReady ();
+			CAN_TxRdy1 = 0;    													/* CAN HW unready to transmit message mailbox 1*/
+					
+			CAN_TxMsg.data[0] = val_Tx;                 /* data[0] = ADC value      */
+			CAN_wrMsg (&CAN_TxMsg);                     /* transmit message         */
+			periodic_modulo++;
+		
+		/*------------------------------------------
+	 * Send an speed frame every 600ms
+	 *-----------------------------------------*/
+		case 1:
+			CAN_TxMsg.id = CAN_ID_SPEED;                /* initialize msg to send   */  
+			for (i = 0; i < 8; i++) CAN_TxMsg.data[i] = 0;
+			CAN_TxMsg.len = 1;
+			CAN_TxMsg.format = STANDARD_FORMAT;
+			CAN_TxMsg.type = DATA_FRAME;	
+		
+			val_Tx++; //Value of the Hall effect sensors
+			CAN_waitReady ();
+			CAN_TxRdy2 = 0;    													/* CAN HW unready to transmit message mailbox 2*/
+					
+			CAN_TxMsg.data[0] = val_Tx;                 /* data[0] = ADC value      */
+			CAN_wrMsg (&CAN_TxMsg);
+			periodic_modulo++;
+			break;
+		
+		case 2:
+			periodic_modulo = 0;
+			break;
+	}		
 }
 
 /*----------------------------------------------------------------------------
   MAIN function
  *----------------------------------------------------------------------------*/
 int main (void)  {
-  int i;
-	
+  
 	Timer_1234_Init (TIM1, 1000000);								/* set Timer 2 every second */
 	Timer_Active_IT(TIM1, 0, canPeriodic);					/* Active Timer2 IT					*/
 	
@@ -128,22 +166,13 @@ int main (void)  {
 
   can_Init ();                                    /* initialize CAN interface */
 
-  CAN_TxMsg.id = 33;                              /* initialize msg to send   */
-  for (i = 0; i < 8; i++) CAN_TxMsg.data[i] = 0;
-  CAN_TxMsg.len = 1;
-  CAN_TxMsg.format = STANDARD_FORMAT;
-  CAN_TxMsg.type = DATA_FRAME;
-
+  
   while (1) {
-    
-    Delay (10);                                   /* delay for 10ms           */
-
+		
     if (CAN_RxRdy) {                              /* rx msg on CAN Ctrl       */
       CAN_RxRdy = 0;
-
       val_Rx = CAN_RxMsg.data[0];
     }
-
     val_display ();                               /* display TX and RX values */
   }
 }
